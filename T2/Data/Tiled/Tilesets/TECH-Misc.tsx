1f9c20a0f3927e568b5487baf88c8dac00790f87
Nicolas <nicolas.braun@ecovium.com>
2025-05-31 10:27:55 +0200

    second map / first transition

---
+++ T2/Data/Tiled/Tilesets/TECH-Misc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TECH-Misc" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../Assets/TECH-Misc.png" width="1024" height="1024"/>
+</tileset>
