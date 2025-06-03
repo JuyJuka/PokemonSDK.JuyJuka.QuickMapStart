5c610c804409b76f251e45eee39f0d2f9477e0b4
Nicolas <nicolas.braun@ecovium.com>
2025-06-03 18:12:25 +0200

    map id

--- PokemonSDK.JuyJuka.QuickMapStart/Program.cs
+++ PokemonSDK.JuyJuka.QuickMapStart/Program.cs
@@ -33,3 +33,29 @@ namespace PokemonSDK.JuyJuka.QuickMapStart
     }
   }
 }
+/*
+    public static void CopyFilesRecursively(string sourcePath, string targetPath, Api.Logging.ILogger logger)
+    {
+      List<Thread> threads = new List<Thread>();
+      Program.CopyFilesRecursively(sourcePath, targetPath, logger, threads);
+      foreach (Thread thread in threads) { thread.Join(); }
+    }
+
+    private static void CopyFilesRecursively(string sourcePath, string targetPath, Api.Logging.ILogger logger, List<Thread> threads)
+    {
+      //Now Create all of the directories
+      foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+      {
+        Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+      }
+
+      //Copy all the files & Replaces any files with the same name
+      foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+      {
+        logger.Write(Path.GetFileName(newPath));
+        Thread t = new Thread(() => File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true));
+        threads.Add(t);
+        t.Start();
+      }
+    }
+ */
